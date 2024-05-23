8cc79fa221e78e9911dd5e18c6f787a928c5c75b
JOK265 <168119292+JOK265@users.noreply.github.com>
2024-05-23 14:18:26 -0300

    Update Operacao com dois numeros e menu.py

--- Operacao com dois numeros e menu.py
+++ Operacao com dois numeros e menu.py
@@ -1,11 +1,10 @@
+try:
+    n1=float(input('Digite um número:'))
+    n2=float(input('Digite outro: '))
+except ValueError:
+    print('Digite apenas valores numéricos.')
 n3=''
 while n3!=5:
-    try:
-        n1=float(input('Digite um número:'))
-        n2=float(input('Digite outro: '))
-    except ValueError:
-        print('Digite apenas valores numéricos.')
-        continue
     soma=n1+n2
     mult=n1*n2
     menu=print('''Agora, nos diga qual operação você quer realizar de acordo com o menu abaixo:
@@ -31,7 +30,12 @@ while n3!=5:
         if n1==n2:
             print('Não há valor maior. Os números são iguais.')
     elif n3==4:
-        continue
-    elif n3!=1 and n3!=2 and n3!=3 and n3!=4 and n3!=5:
+        try:
+            n1=float(input('Digite um número:'))
+            n2=float(input('Digite outro: '))
+        except ValueError:
+            print('Digite apenas valores numéricos.')
+            continue
+    elif n3>5 or n3<1:
         print('Digite uma operação válida!')
 print('---------- PROGRAMA ENCERRADO ----------')
